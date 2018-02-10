e94d82f0a0ef2aada294e2f1cd50e99317e081b0
shashank <shashanksingh1044@gmail.com>
2018-02-10 02:26:40 -0800

    first commit

---
+++ got.js
@@ -0,0 +1,24 @@
+$("#seasons,#seasonsblock").hover(function(){
+	$("#seasonsblock").toggle()
+})
+
+$("#movies,#moviesblock").hover(function(){
+	$("#moviesblock").toggle()
+})
+
+$("#downloads,#downloadsblock").hover(function(){
+	$("#downloadsblock").toggle()
+})
+
+$("#cast,#castblock").hover(function(){
+	$("#castblock").toggle()
+})
+
+$("#gallery,#galleryblock").hover(function(){
+	$("#galleryblock").toggle()
+})
+
+$("#shedule,#sheduleblock").hover(function(){
+	$("#sheduleblock").toggle()
+})
+
